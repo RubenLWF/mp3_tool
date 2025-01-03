1298e8e674f8307aa70d338ab47a2b1a30bf07a0
Ruben Lagerwerf <ruben.lagerwerf@gmail.com>
2025-01-03 14:45:10 +0100

    fix encoding and tag types

--- mp3_editor.py
+++ mp3_editor.py
@@ -1,11 +1,11 @@
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, TextFrame
+from mutagen.id3 import ID3, APIC, TIT2, TPE1
 
 audio = MP3('audio/' + input('Enter .mp3 file name: '), ID3=ID3)
 
-audio.tags['TIT2'] = TextFrame(encoding=3, text=[input('Enter the title: ')])
-audio.tags['TPE1'] = TextFrame(encoding=3, text=[input('Enter the artist: ')])
-audio.tags.add(APIC(encoding=0, mime='image/png', type=3, data=open('img/' + input('Enter .png file name: '), 'rb').read()))
+audio.tags['TIT2'] = TIT2(encoding=3, text=[input('Enter the title: ')])
+audio.tags['TPE1'] = TPE1(encoding=3, text=[input('Enter the artist: ')])
+audio.tags.add(APIC(encoding=3, mime='image/png', type=3, data=open('img/' + input('Enter .png file name: '), 'rb').read()))
 
 audio.save()
 print('Metadata added succesfully!')
